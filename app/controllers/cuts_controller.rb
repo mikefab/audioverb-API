bdf11059f90aaaeb415c067e4ce1c3cc7143e5b9
mikefab <mikefabrikant@gmail.com>
2021-09-26 22:26:15 -0500

    Cuts

--- app/controllers/cuts_controller.rb
+++ app/controllers/cuts_controller.rb
@@ -1,56 +1,48 @@
 class CutsController < ApplicationController
-
+  skip_before_action :verify_authenticity_token
   def cuts_by_nam
     if params[:nam]
       cuts = Cut.where(nam: params[:nam])
       return render json: {
-        cuts: cuts.map{ |cut| {num: cut.num, start: cut.start, stop: cut.stop} }
+        cuts: cuts.map{ |cut| {num: cut.num, start: cut.start, stop: cut.stop, approved: cut.approved} }
       }
     end
     render json: {message: 'empty'}
   end
 
 
+  def approved_user(user_code)
+    return user_code ===  ENV.fetch("USER_CODE") ? true : false
+  end
+
   def save_cut
-    puts ENV.fetch("USER_CODE")
     nam = params[:name].gsub('_', ' ')
     num = params[:num]
     start = params[:start].gsub('-', '.')
     stop = params[:stop].gsub('-', '.')
     user_id = params[:user_code]
-    if ENV.fetch("USER_CODE")===user_id then
-      puts nam
-      nam_id = Nam.find_by(nam: nam).id
-      cap = Cap.find_by(nam_id: nam_id, num: num)
-      cut = Cut.find_by(nam: nam, num: num, user_id: user_id)
-      if !cut and cap then
-        # Cap is being cut for first time. Let anyone do it.
-        Cut.create(cap: cap, start: start, stop: stop, user_id: user_id, nam: nam, num: num, hashed_ip: request.remote_ip.hash)
-      elsif cut && cap
-        # Cap exists. If cut by admin, only update if this cut is submitted by admin
-        if cut.user_id === ENV.fetch("USER_CODE") and user_id ===  ENV.fetch("USER_CODE") then
-          cut.update(start: start, stop: stop)
-        end
-        # Cut is being overwritten by admin's submission
-        if cut.user_id != ENV.fetch("USER_CODE") and user_id ===  ENV.fetch("USER_CODE") then
-          cut.update(start: start, stop: stop)
-        else
-          render json: {error: 'This cut cannot be overwritten.'}
-        end
-      end
-      render json: {message: 'cut updated'}
-    else
-      render json: {error: 'not authorized'}
+    nam_id = Nam.find_by(nam: nam).id
+    cap = Cap.find_by(nam_id: nam_id, num: num)
+    cut = Cut.find_by(nam: nam, num: num)
+    approvable = (user_id === ENV.fetch("USER_CODE")) ? true : false
+
+    if cap and !cut then
+      # Cap is being cut for first time. Let anyone do it.
+      Cut.create(cap: cap, start: start, stop: stop, user_id: user_id || 0, nam: nam, num: num, hashed_ip: request.remote_ip.hash, approved: approvable)
+      return  render json: {message: 'cut updated'}
     end
+    if cap && cut
 
-    #if Rails.cache.exist?("conjugation-#{params[:conjugation]}")
-    #  render json: Rails.cache.read("conjugation-#{params[:conjugation]}")
-    #else
-    #  #verbs = Tense.return_tense_verbs(Tense.where(tense: "#{params[:tense]}").first.id, englishId)
-    #  render json: Rails.cache.fetch("conjugation-#{params[:conjugation]}", :expires_in => 3.days) {
-    #    conjugations.map{ |e|  e[:cla]}
-
-    #  }
-    #end
+      # Cap exists. If cut by admin, only update if this cut is submitted by admin
+      if approved_user(user_id) then
+        cut.update(start: start, stop: stop, approved: approved)
+        return  render json: {message: 'cut updated'}
+      end
+      # Person is not admin, they cannot overwrite an approved cut
+      return render json: {error: 'This cut cannot be overwritten.'} if cut.approved
+      cut.update(start: start, stop: stop, approved: approvable, hashed_ip: request.remote_ip.hash)
+      return  render json: {message: 'cut updated'}
+    end
+      return  render json: {message: 'Cannot create cut'}
   end
 end
